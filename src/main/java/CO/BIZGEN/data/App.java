6d148b53be2d752a23aeb33a1214b495a4d105dc
Raul <raulgcollazo1996@gmail.com>
2016-09-30 13:15:37 +0200

    s

---
+++ src/main/java/CO/BIZGEN/data/App.java
@@ -0,0 +1,13 @@
+package CO.BIZGEN.data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
