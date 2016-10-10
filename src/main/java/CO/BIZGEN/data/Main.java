192d1861955368330d374360986a8abddc81c5cb
OSKAR <OSKAR@172.30.0.84>
2016-10-10 13:35:15 +0200

    commit rama oskar

--- src/main/java/CO/BIZGEN/data/Main.java
+++ src/main/java/CO/BIZGEN/data/Main.java
@@ -1,13 +1,33 @@
 package CO.BIZGEN.data;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
+import java.util.List;
+
+
+
+import CO.BIZGEN.data.dao.UserDAO;
+import CO.BIZGEN.data.dao.impl.HibernateUserDAO;
+
+import CO.BIZGEN.data.model.User;
+
+public class Main {
+
+	/**
+	 * App entry point.
+	 * 
+	 * @param userDAO
+	 */
+	public static void showAll(UserDAO userDAO) {
+		// SELECT ALL DATA
+		List<User> users = userDAO.selectAll();
+		System.out.println("--- CUSTOMER ----- table contents	-----------");
+		for (User customer : users) {
+			System.out.println("Id: " + customer.getId());
+			System.out.println(" - Name: " + customer.getUsername());
+			System.out.println(" - Password: " + customer.getPassword());
+		}
+		System.out.println("Total Users: " + users.size());
+	}
+	public static void main(String[] args) {
+		UserDAO userDAO = new HibernateUserDAO();
+		showAll(userDAO);
+	}}
